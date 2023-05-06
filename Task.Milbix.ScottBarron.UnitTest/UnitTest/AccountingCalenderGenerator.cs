6575a179af068a43ee00db5f16be90c47f5f89fe
ahmedlutfulzamil@gmail.com <Rihan@08102018>
2023-05-06 21:11:44 +0600

    Unit Test Project Included and started implementing

---
+++ Task.Milbix.ScottBarron.UnitTest/UnitTest/AccountingCalenderGenerator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Task.Milbix.ScottBarron.DAL;
+
+namespace Task.Milbix.ScottBarron.UnitTest.UnitTest
+{
+    public class AccountingCalenderGenerator
+    {
+        [Fact]
+        public void Generate_GenerateAccountingCalendarViewModel_PeriodsMustNotBeEmpty()
+        {
+            //Arrange
+            var accountingCalendarGenerator = new AccountingCalendarGenerator();
+
+            DateTime startDate = new DateTime(2020, 7, 2);
+            DateTime endDate = new DateTime(2023, 6, 30);
+
+            //Act
+
+            IEnumerable<IPeriod> periods = accountingCalendarGenerator.Generate(startDate, endDate);
+
+            //Assert
+            Assert.NotEmpty(periods);
+        }
+
+        [Fact]
+        public void Generate_GenerateAccountingCalendarViewModel_ReturnTypeMustBeAssignableFormIEnumerableOfIPeriod()
+        {
+            //Arrange
+            var accountingCalendarGenerator = new AccountingCalendarGenerator();
+
+            DateTime startDate = new DateTime(2020, 7, 2);
+            DateTime endDate = new DateTime(2023, 6, 30);
+
+            //Act
+
+            var periods = accountingCalendarGenerator.Generate(startDate, endDate);
+
+            //Assert
+            Assert.IsAssignableFrom<IEnumerable<IPeriod>>(periods);
+        }
+    }
+
+
+}
---
+++ Task.Milbix.ScottBarron.UnitTest/UnitTest/GroupPeriodsByFinancialYear.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task.Milbix.ScottBarron.DAL;
+
+namespace Task.Milbix.ScottBarron.UnitTest.UnitTest
+{
+    public class GroupPeriodsByFinancialYear
+    {
+        [Fact]
+        public void AccountingPeriodGrouper_GroupFinancialPerioInGroip_ReturnTypeMustBeAssignableFormDictionaryByKeyIFinancialYear()
+        {
+            //Arrange
+            var accountingCalendarGenerator = new AccountingCalendarGenerator();
+            AccountingPeriodGrouper accountingPeriodGrouper = new AccountingPeriodGrouper();
+
+            DateTime startDate = new DateTime(2020, 7, 2);
+            DateTime endDate = new DateTime(2023, 6, 30);
+
+            //Act
+
+            var periods = accountingCalendarGenerator.Generate(startDate, endDate);
+            var groupedPeriods = accountingPeriodGrouper.GroupPeriodsByFinancialYear(periods);
+            //Assert
+            Assert.IsAssignableFrom<Dictionary<IFinancialYear, List<IPeriod>>>(groupedPeriods);
+        }
+    }
+}
